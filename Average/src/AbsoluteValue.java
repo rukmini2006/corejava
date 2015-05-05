b36f536b4afec57265eb4f085e04f04440b61dfd
rukmini2006 <rukmini.mortha@gmail.com>
2015-05-04 22:47:25 -0400

    absolute value changed from 10 to 20

--- Average/src/AbsoluteValue.java
+++ Average/src/AbsoluteValue.java
@@ -3,7 +3,7 @@ public class AbsoluteValue {
 
 	public static void main(String[] args) {
 		double value=Math.abs(-10);
-		double value1=Math.abs(1-10);
+		double value1=Math.abs(1-20);
 		System.out.println("value is: "+value);
 		System.out.println(" value1 is: "+value1);
 
