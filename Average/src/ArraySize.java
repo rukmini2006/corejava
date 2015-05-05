4111645e5ad242bf64363188f666e4dcb53b3a56
rukmini2006 <rukmini.mortha@gmail.com>
2015-05-04 22:52:24 -0400

    Array size is changed

--- Average/src/ArraySize.java
+++ Average/src/ArraySize.java
@@ -2,7 +2,7 @@
 public class ArraySize {
 
 	public static void main(String[] args) {
-	int[] array={1,2,3,4};
+	int[] array={1,2,3,4,5};
 	String[] names={"ram","roja"};
 	System.out.println(" size of array is "+array.length);
 	System.out.println("size of String array is:  "+names.length);
