2f46be4fe3bde679d6ecc3a58657ebf3c7307ab4
rukmini2006 <rukmini.mortha@gmail.com>
2015-05-11 17:01:39 -0400

    update

--- FirstProject/src/PrintingName.java
+++ FirstProject/src/PrintingName.java
@@ -4,7 +4,7 @@ public class PrintingName {
 	public static void main(String[] args) {
 		String firstName = "Rukmini";
 		String lastName = "Mortha";
-		System.out.println("Printing name as: "+firstName  + " " + lastName);
+		System.out.println("Printing name as: "+ firstName  + " " + lastName);
 	
 
 	}
