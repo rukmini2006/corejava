a10ea5ff61d093e5e5dea560750257ba208654df
rukmini2006 <rukmini.mortha@gmail.com>
2015-05-11 16:50:16 -0400

    up

---
+++ FirstProject/src/PrintingName.java
@@ -0,0 +1,14 @@
+
+public class PrintingName {
+
+	public static void main(String[] args) {
+		try{
+		int name = Integer.parseInt(args[0]);
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		
+
+	}
+
+}
