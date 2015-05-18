c4574b25be66d791305b1a5bcb011f50fc6cfe97
rukmini2006 <rukmini.mortha@gmail.com>
2015-05-18 11:03:33 -0400

    update

--- Average/src/arrays/ArraysExampleForSingleDimension.java
+++ Average/src/arrays/ArraysExampleForSingleDimension.java
@@ -13,16 +13,24 @@ public class ArraysExampleForSingleDimension {
 
 		System.out.println("Using normal for loop=============== ");
 		for (int i = 0; i < ages.length; i++)
-			System.out.println("elements of ages array :  " +ages[i]);
+			System.out.println("elements of ages array :  " + ages[i]);
 
 		System.out.println("Using Enhanced for loop=============== ");
 		for (int a : ages) {
 			System.out.println(a);
+
+			String names[] = { "one", "two", "three" };
+
+			System.out.println("Normal for loop ");
+			for (int i = 0; i < names.length; i++)
+				System.out.println("elements are : " + names[i]);
+			System.out.println("\n============== ");
+
+			System.out.println("Enhanced for loop ");
+			for (String str : names)
+				System.out.println("elements are : " + str);
+
 		}
-
-		// ages[0] = 60;
-
-		// ages[1] = 20;
 
 	}
 
