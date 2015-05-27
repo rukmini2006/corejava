0ddb7147dcfaf73f2681f0e48caecab2220f8da1
rukmini2006 <rukmini.mortha@gmail.com>
2015-05-27 17:23:55 -0400

    updatet

---
+++ Average/src/Testing.java
@@ -0,0 +1,18 @@
+/**
+ * This class is just a testing class to show the comments
+ * @author rukmini.mortha
+ *
+ */
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * This method is created to be called
+	 */
+public void callMe(){
+	
+}
+}
