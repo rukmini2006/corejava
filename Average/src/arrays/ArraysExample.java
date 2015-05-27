ea94971261b3f6a60b5a1aa398a02fdfe151cd80
rukmini2006 <rukmini.mortha@gmail.com>
2015-05-27 17:57:14 -0400

    update

--- Average/src/arrays/ArraysExample.java
+++ Average/src/arrays/ArraysExample.java
@@ -1,23 +1,26 @@
 package arrays;
+
 /**
- * This is  an array example
+ * This is one dimension array and two dimension array
  * @author rukmini.mortha
  *
  */
 public class ArraysExample {
 
-	public static void oneDimen() {
-		int age1 = 100;
+	/**
+	 * This is a one Dimension Array
+	 */
+	public static void oneDimenArray() {
+		int age1 = 10;
 		int age2 = 20;
 		int age3 = 30;
-
+		
 		int ages[] = new int[3]; // {10,20,30};
 		ages[0] = age1;
 		ages[1] = age2;
 		ages[2] = age3;
 
 		// ages[3] = 40;
-		// System.out.println("Length is :  "+ages.length);
 
 		System.out.println(" Length is " + ages.length);
 
@@ -27,35 +30,65 @@ public class ArraysExample {
 
 		// System.out.println(ages[3] + " ");
 
-		String names[] = { "Jayram", "Manasa", "Rajeev" };
-		System.out.println("String length is :  " + names.length);
-		System.out.println(names[0]);
+		// String names[] = {"Jayram", "Manasa", "Rajeev"};
+		// System.out.println(names[0]);
 
+		System.out.println("\n=============");
+
+		for (int i = 0; i < ages.length; i++) {
+			System.out.println(ages[i] + " ");
+		}
+
+		String names[] = { "one", "two", "three" };
+		
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+		
+		// Enhanced for loop. For each
+		for(String name : names) {
+			System.out.println(name);
+		}
+		
+		for(int age : ages) {
+			System.out.println(age);
+		}
+		
 	}
-
-	public static void twoDimen() {
-		// declaring
-		// initializing
-
+	/**
+	 * This is two dimension array
+	 */
+	
+	public static void twoDimenArray() {
 		int person1[] = { 5, 30 }; // height and age and percentage
 		int person2[] = { 6, 32 }; // height and age
 		int person3[] = { 7, 34 }; // height and age
 
-		int persons[][] = { person1, person2, person3 };
-
 		System.out.println(person1[0] + "  " + person1[1]);
 		System.out.println(person3[0] + "  " + person3[1]);
+		
+		int persons[][] = { person1, person2, person3 };
 
-		System.out.print(persons[2][0] + " ");
-		System.out.print(persons[2][1] + " ");
-
-		System.out.println("\n=============");
-
+		System.out.println(persons[0][0] + " " + persons[0][1]);
+		System.out.println(persons[1][0] + " " + persons[1][1]);
+		
+		System.out.println(" persons.length "+persons.length);
+		
+		for(int i = 0 ; i < persons.length ; i++) {
+			System.out.println(persons[i][0] + " " + persons[i][1]);
+		}
+		
+		System.out.println("=======================");
+		
+		for(int i = 0 ; i < persons.length ; i++) {
+			for(int  j = 0; j < persons[i].length ; j++) {
+				System.out.print(persons[i][j] + " " + persons[i][j]);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		// oneDimen();
-		twoDimen();
+//		oneDimenArray();
+		twoDimenArray();
 	}
-
 }
