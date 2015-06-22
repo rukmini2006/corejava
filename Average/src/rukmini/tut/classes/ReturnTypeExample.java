a547c34aa6705ce7ceaad9ca1d190bf61737fec3
rukmini2006 <rukmini.mortha@gmail.com>
2015-06-22 03:17:32 -0400

    update

--- Average/src/rukmini/tut/classes/ReturnTypeExample.java
+++ Average/src/rukmini/tut/classes/ReturnTypeExample.java
@@ -1,16 +1,22 @@
 package rukmini.tut.classes;
 
 public class ReturnTypeExample {
+	int value;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
 		ReturnTypeExample rte = new ReturnTypeExample();
+		//int d;
+		rte.setInitialValue(9);
 		rte.doCalculation(3,5);
 		rte.doCalculation(4, 5,7);
 		
+		
 		String dou = rte.add(4, 6) + " ";
-
-
+		
+		
 	}
 	
 	public String getName(String firstName,String lastName){
@@ -32,7 +38,7 @@ public class ReturnTypeExample {
 	public void doCalculation(int a,int b) {
 		//ReturnTypeExample rte = new ReturnTypeExample();
 
-		int finalValue = 8 - add(a,b);
+		int finalValue = value - add(a,b);
 		System.out.println(finalValue);
 	}
 	
@@ -41,8 +47,12 @@ public class ReturnTypeExample {
 		//ReturnTypeExample rte = new ReturnTypeExample();
 
 		
-		float finalValue = 8- add(a,b,c);
+		float finalValue = value - add(a,b,c);
 		System.out.println(finalValue);
+	}
+
+	public  void setInitialValue(int val) {
+		this.value = val;
 	}
 
 	
