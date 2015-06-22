bc6d87dfe7eb19009f15e76b9ee1c608ef21c05b
rukmini2006 <rukmini.mortha@gmail.com>
2015-06-22 07:44:26 -0400

    update

---
+++ Average/src/rukmini/tut/classes/access/Student.java
@@ -0,0 +1,11 @@
+package rukmini.tut.classes.access;
+
+public class Student {
+	public static void main(String[] args) {
+		Student studfive =new Student();
+		System.out.println(studfive);
+
+	
+	}
+
+}
