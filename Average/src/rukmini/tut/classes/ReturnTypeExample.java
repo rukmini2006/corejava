3fe0e5f5a2fcff894072bec785d46612a14655d3
rukmini2006 <rukmini.mortha@gmail.com>
2015-06-22 03:48:13 -0400

    update

--- Average/src/rukmini/tut/classes/ReturnTypeExample.java
+++ Average/src/rukmini/tut/classes/ReturnTypeExample.java
@@ -1,60 +1,39 @@
 package rukmini.tut.classes;
 
 public class ReturnTypeExample {
-	int value;
+	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
 		ReturnTypeExample rte = new ReturnTypeExample();
+		Calculation cal = new Calculation();
+		cal.add(4, 5);
+		cal.doCalculation(2, 3);
 		//int d;
-		rte.setInitialValue(9);
+		/*rte.setInitialValue(9);
 		rte.doCalculation(3,5);
 		rte.doCalculation(4, 5,7);
+		*/
 		
+		//String dou = rte.add(4, 6) + " ";
 		
-		String dou = rte.add(4, 6) + " ";
+		/*System.out.println("Hello ");
+		String name  = rte.getName("Dinesh","kumar");
+		System.out.println("How u doing ");
+		*/
 		
+		System.out.println("Hello " + rte.getName("Dinesh", "kumar")+"How r u ding?");
 		
 	}
 	
 	public String getName(String firstName,String lastName){
+		System.out.println(firstName + " ******"+ lastName);
 		return firstName + " ******"+ lastName;
 		
-	}
-
-	public int add(int i, int j) {
-		// System.out.println(i+j);
-		return (i + j);
-	}
-	
-	public float add(int i,int j,int k){
-		return (float) (i+j+k);
 		
 	}
-	
-
-	public void doCalculation(int a,int b) {
-		//ReturnTypeExample rte = new ReturnTypeExample();
-
-		int finalValue = value - add(a,b);
-		System.out.println(finalValue);
-	}
-	
-	
-	public void doCalculation(int a,int b,int c) {
-		//ReturnTypeExample rte = new ReturnTypeExample();
-
-		
-		float finalValue = value - add(a,b,c);
-		System.out.println(finalValue);
-	}
-
-	public  void setInitialValue(int val) {
-		this.value = val;
-	}
 
 	
-
 }
