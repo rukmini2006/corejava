d9734de35d237f45253730cb1b39d1fe2b5d358f
rukmini2006 <rukmini.mortha@gmail.com>
2015-06-23 03:04:36 -0400

    update

---
+++ Average/src/rukmini/tut/classes/ToStringExample.java
@@ -0,0 +1,15 @@
+package rukmini.tut.classes;
+
+public class ToStringExample {
+
+	public static void main(String[] args) {
+		Student stud = new Student("jayram",4);
+		System.out.println(stud);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Average/src/rukmini/tut/classes/VariableExample2.java
@@ -0,0 +1,20 @@
+package rukmini.tut.classes;
+
+import rukmini.tut.classes.Student;
+
+
+
+public class VariableExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//VariableExample2 val2 =new VariableExample2();
+		/*Student stud = new Student("ruk",8);
+		System.out.println(Student.count);*/
+		Student.sayHello();
+		
+}
+
+	
+}
+
