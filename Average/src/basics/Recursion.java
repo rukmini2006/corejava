d7d63d9f08d7714d0c1018613a1f20e54946dc6d
rukmini2006 <rukmini.mortha@gmail.com>
2015-06-23 10:34:32 -0400

    update

---
+++ Average/src/basics/Recursion.java
@@ -0,0 +1,29 @@
+package basics;
+
+class Factorial{
+	int factorial(int n){
+		int val = 0;
+		int result;
+		if(n==1)
+		return 1;
+		
+		result = factorial(n-1) *n;
+		return result;
+		
+	}
+	
+}
+
+public class Recursion {
+public static void main(String[] args) {
+	
+	Factorial f = new Factorial();
+	System.out.println("Factorial of 4 is: "+f.factorial(7));
+	/*int fact = 1;
+	for(int i =1;i<=5;i++){
+		fact = fact*i;
+		
+	}
+	System.out.println("Factorial of the given number is :  "+fact);*/
+}
+}
