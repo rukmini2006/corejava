cdae0db155fb833b4b36b9477e9f718f97727e8c
rukmini2006 <rukmini.mortha@gmail.com>
2015-06-26 03:30:26 -0400

    update

--- Average/src/rukmini/tut/classes/innerclass/House.java
+++ Average/src/rukmini/tut/classes/innerclass/House.java
@@ -3,10 +3,17 @@ package rukmini.tut.classes.innerclass;
 public class House {
 	
 	String name = "y ";
+	public House(String name){
+		this.name = name;
+	}
 	
-     class HousePainter{
+     public class HousePainter{
+    	 String color;
+    	 public HousePainter(String color){
+    		 this.color = color;
+    	 }
     	 public void painter() {
-    		 System.out.println(" Iam painting");
+    		 System.out.println(" Iam painting  " +name +"  House with  "+color+ "  color");
     	 }
      }
 	
--- Average/src/rukmini/tut/classes/innerclass/InnerClassExample.java
+++ Average/src/rukmini/tut/classes/innerclass/InnerClassExample.java
@@ -7,11 +7,12 @@ public class InnerClassExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Calculation calc = new Calculation();
-		House house  =new House();
+		House house  =new House("bag");
 		System.out.println(house.name);
 		
-		HousePainter hp = house.new HousePainter();
-		HousePainter hp1 = new House().new HousePainter();
+		HousePainter hp = house.new HousePainter("black");
+		HousePainter hp1 = new House("rukmini").new HousePainter("blue");
+		hp1.painter();
 		
 		System.out.println(hp);
 		System.out.println(hp1);
