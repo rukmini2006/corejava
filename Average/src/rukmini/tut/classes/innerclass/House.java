1fea6f050d7e93bd4d8f3c3e891c0f7a8d3614fc
rukmini2006 <rukmini.mortha@gmail.com>
2015-06-26 02:56:10 -0400

    update

---
+++ Average/src/rukmini/tut/classes/innerclass/House.java
@@ -0,0 +1,17 @@
+package rukmini.tut.classes.innerclass;
+
+public class House {
+	
+	String name = " ";
+	
+     class HousePainter{
+    	 public void painter() {
+    		 System.out.println(" Iam painting");
+    	 }
+     }
+	
+	public void paint() {
+		
+	}
+	
+}
