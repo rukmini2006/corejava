f06c283cf7dc10ad84fab228732c1ba9aa8ca858
rukmini2006 <rukmini.mortha@gmail.com>
2015-07-14 08:30:05 -0400

    update

--- Average/src/rukmini/tutorial/exception/ExceptionExample.java
+++ Average/src/rukmini/tutorial/exception/ExceptionExample.java
@@ -1,20 +1,43 @@
 package rukmini.tutorial.exception;
 
+import rukmini.tut.inheritance.itemp.JavaEmp;
+
 public class ExceptionExample {
 
 	public static void main(String[] args) {
+		/*String name = null;
+		System.out.println(name.length());
 		// TODO Auto-generated method stub
-		int index = Integer.parseInt(args[0]);
+		*/
+		JavaEmp emp = null  ;
+	//	System.out.println(emp!= null? emp.toString():" ");
+		printEmp(emp);
+		try {
+			int  index = Integer.parseInt("2");
+		} catch (Exception e1) {
+			int index = 1000;
+			e1.printStackTrace();
+		}
 		int ages [] = {23,45,67,78,100};
+		int randomvalue[] = {1,3,0,5,8};
+		
 		System.out.println("Before ");
-		int age;
+		int age = 0,value;
 			//	System.out.println(age);
 		System.out.println("After ");
 		try{
-			age = ages[index];
+			int index =  Integer.parseInt("2");
+			age = ages[index ];
+			value= randomvalue[index];
+			int someValue = age/value;
+			//close connection;
 		}catch (ArrayIndexOutOfBoundsException e){
 			e.printStackTrace();
 			age = 1000;
+		}catch(ArithmeticException e){
+			e.printStackTrace();
+		}finally{
+			System.out.println("Iam the finaslly block ");
 		}
 		System.out.println(age);
 		if(age==1000) {
@@ -24,7 +47,19 @@ public class ExceptionExample {
 		}else if(age <1){
 			System.out.println("Iam kid ");
 		}
+		try {
+			System.out.println("Division is"+Division.divideValues(12,0 ));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Iam at the final stage");
 
 	}
+	public static void printEmp(JavaEmp emp){
+		System.out.println(emp != null ?emp.toString():" ");
+	}
+	
+	
 
 }
