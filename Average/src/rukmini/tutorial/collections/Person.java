0734e6e564edd5e38d954367280c4d5f1cbb2e30
rukmini2006 <rukmini.mortha@gmail.com>
2015-07-25 19:50:12 -0400

    update

--- Average/src/rukmini/tutorial/collections/Person.java
+++ Average/src/rukmini/tutorial/collections/Person.java
@@ -1,5 +1,26 @@
 package rukmini.tutorial.collections;
 
 public class Person {
+String name;
+String Profession;
 
+public Person(){
+	
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getProfession() {
+	return Profession;
+}
+
+public void setProfession(String profession) {
+	Profession = profession;
+}
 }
