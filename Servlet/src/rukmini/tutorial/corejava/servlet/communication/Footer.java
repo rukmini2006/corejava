b349149f87557398190f1509cd078bf54ed524e3
rukmini2006 <rukmini.mortha@gmail.com>
2015-08-06 13:19:35 -0400

    COMMUNICATION SERVLET(for servlet communication also known as servlet collabaration)

---
+++ Servlet/src/rukmini/tutorial/corejava/servlet/communication/Footer.java
@@ -0,0 +1,39 @@
+package rukmini.tutorial.corejava.servlet.communication;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Header
+ */
+@WebServlet("/Footer")
+public class Footer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Footer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
