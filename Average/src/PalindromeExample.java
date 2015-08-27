6713be724b643034360680a8e74909f062c26e1d
rukmini2006 <rukmini.mortha@gmail.com>
2015-08-27 17:07:21 -0400

    update

---
+++ Average/src/PalindromeExample.java
@@ -0,0 +1,20 @@
+
+public class PalindromeExample {
+
+	public static void main(String[] args) {
+		int rem,sum=0,temp;
+		int num =454;
+		temp=num;
+		while(num>0){
+			rem =num%10;
+			sum =(sum*10)+rem;
+			num =num/10;
+		}
+		if(temp==sum){
+			System.out.println("Palindrome number ");
+		}else
+			System.out.println(" Not palindrome");
+
+	}
+
+}
